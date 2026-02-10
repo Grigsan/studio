54ae92247c921cf31368bc78fcaf67d87efbac69
Elois <irisemo146@gmail.com>
2026-02-10 06:09:09 +0000

    Try fixing this error: `Runtime FirebaseError: Firebase: Error (auth/con

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,7 +1,15 @@
 import { getApps, initializeApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
 import { getFirestore, type Firestore } from 'firebase/firestore';
-import { firebaseConfig } from './config';
+
+const firebaseConfig = {
+  "projectId": "studio-6463950600-edbff",
+  "appId": "1:267858110872:web:9dcb9b0ab7fd565a82a3ed",
+  "apiKey": "AIzaSyBjlPR841YvcLvKNnbBxNakxDiExjj7B8A",
+  "authDomain": "studio-6463950600-edbff.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "267858110872"
+};
 
 function getFirebase() {
   const apps = getApps();
